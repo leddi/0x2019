a4522f5d45b144db28c399aeb9298a55b9ed96e7
leddi <peter@ledderboge.net>
2019-11-09 22:53:37 +0100

    typo

--- hello.py
+++ hello.py
@@ -14,6 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
